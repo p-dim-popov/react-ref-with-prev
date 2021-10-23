038fd5e841119da138a1ae11eff9e34381412dad
Peter Popov <p.dim.popov@gmail.com>
2021-10-23 08:53:47 +0300

    refactor(createRefWithPrev): better interfacing for js

--- src/createRefWithPrev.ts
+++ src/createRefWithPrev.ts
@@ -1,36 +1,38 @@
-import { RefObject } from 'react';
+type Callable<T> = ((newRef?: T) => void);
 
-type BaseRefObjectWithPrev<T> = RefObject<T> & ((newRef?: T) => void);
-
-export interface RefObjectWithPrev<T> extends BaseRefObjectWithPrev<T> {
-    readonly prev: T | null;
+export interface RefObjectWithPrev<T> extends Callable<T> {
+  readonly prev: T | null;
+  readonly current: T | null;
 }
 
 export interface IOptions<T = any> {
-  initialValue?: T;
+  initialValue?: T | null;
   onChange?: Function;
   onBeforeChange?: Function;
 }
 
 export const createRefWithPrev:
-  (<T> (onChange?: Function) => RefObjectWithPrev<T>)
-  & (<T> (options?: IOptions<T>) => RefObjectWithPrev<T>)
-  = <T> (
+(<T> (onChange?: Function) => RefObjectWithPrev<T>)
+& (<T> (options?: IOptions<T>) => RefObjectWithPrev<T>) = <T> (
   onChange?: IOptions<T> | Function,
 ): RefObjectWithPrev<T> => {
-  const _options = typeof onChange === 'function' ? { onChange } : onChange;
+  const _options = typeof onChange === 'function' ? { onChange } : onChange ?? {};
 
-  const _onBeforeChange = _options?.onBeforeChange ?? (() => {});
-  const _onChange = _options?.onChange ?? (() => {});
+  const _onBeforeChange = _options.onBeforeChange ?? (() => {});
+  const _onChange = _options.onChange ?? (() => {});
 
-  const set = ((ref: T) => {
+  const _state = {
+    current: _options.initialValue ?? null,
+    prev: null as typeof _options.initialValue,
+  };
+
+  return Object.defineProperties(((ref: T) => {
     _onBeforeChange();
-    set.prev = set.current ?? null;
-    set.current = ref ?? null;
+    _state.prev = _state.current ?? null;
+    _state.current = ref ?? null;
     _onChange();
-  }) as any;
-  set.current = _options?.initialValue ?? null;
-  set.prev = null;
-
-  return set as RefObjectWithPrev<T>;
+  }), {
+    current: { get() { return _state.current; } },
+    prev: { get() { return _state.prev; } },
+  }) as RefObjectWithPrev<T>;
 };
